2df646d59766b4dad05af5fe47da89b6b7706109
Patrik Spathon <patrik@spathon.com>
2019-10-09 16:02:26 +0200

    Create start page and add socket.io

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
 
 const app = express()
+const httpServer = http.createServer(app)
+const io = socketIo(httpServer)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const server = app.listen(5555, () => {
-    console.log(`Express ready on ${5555}`)
+let players = []
+
+io.on('connection', (socket) => {
+  console.log('Connected:', socket.id)
+  players.push(socket.id)
+  socket.on('disconnect', () => {
+    console.log('Disconnected:', socket.id)
+    const filtered = players.filter(p => p !== socket.id)
+    players = [...filtered]
+  })
+})
+
+const server = httpServer.listen(5555, () => {
+  console.log(`Express ready on ${5555}`)
 })
---
+++ public/js/script.js
@@ -0,0 +1,57 @@
+import {
+  Hexa, Point, Hex
+} from '../hex.js'
+
+const gameEl = document.getElementById('game')
+
+// Fixed variables
+const SIZE = 15
+const SPACING = 4
+const TOTAL_SIZE = SIZE + SPACING
+const grid = []
+const WIDTH = gameEl.offsetWidth - 2
+const HEIGHT = gameEl.offsetHeight - 2
+const HEX_WIDTH = TOTAL_SIZE * 2
+const HEX_HEIGHT = Math.sqrt(3) * TOTAL_SIZE
+const HEX_SPACING = HEX_WIDTH * 3/4
+const ITEMS_WIDTH = WIDTH / HEX_SPACING + 1
+const ITEMS_HEIGHT = HEIGHT / HEX_HEIGHT + 1
+
+
+const canvas = document.getElementById('background')
+canvas.height = HEIGHT
+canvas.width = WIDTH
+const ctx = canvas.getContext('2d')
+ctx.fillStyle = 'rgba(0, 0, 0, .5)'
+ctx.strokeStyle = '#007bd2'
+ctx.lineWidth = 1
+
+
+
+
+const hexa = new Hexa({ size: SIZE, spacing: SPACING })
+
+function draw(cube, color) {
+  const center = hexa.flatHexToPixel(cube)
+  const corners = hexa.getCorners(center)
+  ctx.beginPath()
+  ctx.moveTo(corners[0].x, corners[0].y)
+
+  for (let i = 1; i < corners.length; i++) {
+    ctx.lineTo(corners[i].x, corners[i].y)
+  }
+
+  ctx.closePath()
+  ctx.stroke()
+  if (color) ctx.fillStyle = color
+  ctx.fill()
+}
+
+
+for (let iY = 0; iY < ITEMS_HEIGHT; iY++) {
+  for (let iX = 0; iX < ITEMS_WIDTH; iX++) {
+    const cube = hexa.oddqToCube(new Point(iX, iY))
+    grid.push(cube)
+    draw(cube)
+  }
+}
