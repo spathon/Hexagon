0df4db28018b943d6e4a882961cf95da87aa3f2b
Patrik Spathon <patrik@spathon.com>
2019-10-09 16:32:22 +0200

    Simple create game

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
+const generate = require('project-name-generator')
 
 const app = express()
 const httpServer = http.createServer(app)
@@ -10,6 +11,15 @@ const io = socketIo(httpServer)
 app.use(express.static(path.join(__dirname, 'public')))
 
 let players = []
+const games = {}
+
+// @todo max recursion
+function generateGameId() {
+  const gemaId = generate().dashed
+  if (games[gemaId]) return generateGameId()
+  return gemaId
+}
+
 
 io.on('connection', (socket) => {
   console.log('Connected:', socket.id)
@@ -18,6 +28,16 @@ io.on('connection', (socket) => {
     console.log('Disconnected:', socket.id)
     const filtered = players.filter(p => p !== socket.id)
     players = [...filtered]
+  })
+
+  socket.on('START_GAME', (msg) => {
+    const gameId = generateGameId()
+    games[gameId] = {
+      createdAt: Date.now(),
+      players: [{ id: socket.id, username: msg.username }]
+    }
+    console.log(games)
+    return gameId
   })
 })
 
--- public/js/game.js
+++ public/js/game.js
@@ -1,2 +1,11 @@
 
 const socket = io()
+
+
+const startForm = document.getElementById('startGameForm')
+const usernameInput = document.getElementById('username')
+
+startForm.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+  socket.emit('START_GAME', { username: usernameInput.value })
+})
